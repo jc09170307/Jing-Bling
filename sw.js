fb7e0aab2967d6aa1d63be475f5234e88cd6dc00
jc09170307 <joncenteno92@gmail.com>
2026-07-03 03:58:24 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='jingbling-v1';
+self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r&&r.status===200){const c=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,c));}return r;}).catch(()=>caches.match(e.request)));});
